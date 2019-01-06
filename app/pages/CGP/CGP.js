ef9f0e2f3bf3cfc9fec362b022b3dc082af1c0dc
Cremba <david@brima.it>
2019-01-06 16:25:25 +0200

    organized top 5 boxes and cgp form

--- app/pages/CGP/CGP.js
+++ app/pages/CGP/CGP.js
@@ -132,10 +132,11 @@ class CGP extends Component<Props> {
     } = this.props
     return (
       <Flexbox>
-        <Flexbox className="vote-box" flexDirection="column">
-          <label className="vote-title">Vote for next distribution</label>
-          <label className="address">Proposal Address</label>
-          <Flexbox flexDirection="column">
+        <Flexbox className="vote-box" flexDirection="column" >
+          <h3 className="vote-title">Vote for next distribution</h3>
+
+          <Flexbox flexDirection="column" className="form-row">
+            <label className="address">Proposal Address</label>
             <input
               id="payoutAddress"
               ref={ref('elTo').bind(this)}
@@ -154,6 +155,9 @@ class CGP extends Component<Props> {
               isHidden={!payoutAddress}
             />
             {this.renderAddressErrorMessage()}
+          </Flexbox>
+
+          <Flexbox flexDirection="column" className="form-row">
             <AmountInput
               amount={payoutAmount}
               amountDisplay={payoutAmount}
@@ -166,6 +170,10 @@ class CGP extends Component<Props> {
               label="Amount"
               classname="amount"
             />
+          </Flexbox>
+
+          <Flexbox flexDirection="row" justifyContent="flex-end" className="form-row button-row">
+
             <ProtectedButton
               className={cx('button-on-right', { loading: inprogress })}
               disabled={this.isSubmitButtonDisabled}
@@ -173,7 +181,9 @@ class CGP extends Component<Props> {
             >
               {inprogress ? 'Voting' : 'Vote'}
             </ProtectedButton>
+
           </Flexbox>
+
         </Flexbox>
       </Flexbox>
     )
@@ -257,14 +267,13 @@ class CGP extends Component<Props> {
             <BoxLabel firstLine={`${fund ? kalapasToZen(fund) : 0} ZP`} secondLine="Available for next distribution" />
             <BoxLabel firstLine={`${totalPayoutAmountVoted ? kalapasToZen(totalPayoutAmountVoted) : 0} ZP`} secondLine="Voted for next distribution" />
             <BoxLabel firstLine={this.calcRemainingBlock()} secondLine="Blocks remaining" />
-            <Flexbox flexGrow={1} />
           </Flexbox>
           <Flexbox flexDirection="row" >
-            <Flexbox flexDirection="column" >
+            <Flexbox flexDirection="column" flexGrow={1} >
               { this.renderVote() }
               { this.renderResult() }
             </Flexbox>
-            <Flexbox className="active-proposal">
+            <Flexbox className="active-proposal" flexGrow={1} >
               <table>
                 <thead>
                   <tr>
